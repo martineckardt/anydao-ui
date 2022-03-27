eefd2346cae2abc5519d62799603c26c8fa7c21a
Martin Eckardt <m.eckardt@outlook.com>
2022-03-27 11:10:03 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import useConnect from './hooks/useConnect';
 import useCastVote from './hooks/useCastVote';
-import useERC20Contract from './hooks/useERC20';
+import useERC20Contract from './hooks/useERC20Contract';
 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
